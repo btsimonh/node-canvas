d9a637a4c0bfa41453bef4386838b233308123bc
Simon Hailes <simon@yellaumbrella.tv>
2024-11-01 16:07:53 +0000

    add diagnostics - why is windows taking so long?

--- prebuild/Windows/bundle.sh
+++ prebuild/Windows/bundle.sh
@@ -1,9 +1,11 @@
 # remove the big artifacts we will not use.
 # on windows this is >45mbytes.
+echo start delete object files
 rm -r build/Release/obj
 rm -r build/Release/canvas.exp
 rm -r build/Release/canvas.iobj
 rm -r build/Release/canvas.ipdb
 rm -r build/Release/canvas.lib
 rm -r build/Release/canvas.pdb
+echo done delete object files
 
